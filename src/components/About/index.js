91bac0d10e28b9c8a715c814501922d639842f88
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2022-06-09 19:16:20 +0530

    Add files via upload

---
+++ src/components/About/index.js
@@ -0,0 +1,86 @@
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import {
+  faPython,
+  faCss3,
+  faGitAlt,
+  faHtml5,
+  faJsSquare,
+  faReact,
+} from '@fortawesome/free-brands-svg-icons'
+import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import './index.scss'
+
+const About = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  useEffect(() => {
+    return setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+  }, [])
+
+  return (
+    <>
+      <div className="container about-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
+              idx={15}
+            />
+          </h1>
+          <p>
+            I'm very ambitious front-end developer looking for a role in
+            established IT company with the opportunity to work with the latest
+            technologies on challenging and diverse projects.
+          </p>
+          <p align="LEFT">
+          Looking for a role in a reputable organization to utilize my technical, database, and management
+          skills for the growth of the organization as well as to enhance my knowledge about new and
+          emerging trends in the IT sector
+          </p>
+          {/* <p>
+            If I need to define myself in one sentence that would be a family
+            person, father of a beautiful daughter, a sports fanatic,
+            photography enthusiast, and tech-obsessed!!!
+          </p> */}
+          <div  ><button className="flat-button"
+            
+          onClick={() => window.open("https://drive.google.com/file/d/1ZYLE2YP2qja9iN0HDhqCfTfGMePSzJ0o/view?usp=sharing")}
+          >Download Resume </button></div>
+        </div>
+        
+
+        <div className="stage-cube-cont">
+          <div className="cubespinner">
+            <div className="face1">
+              <FontAwesomeIcon icon={faPython} color="#DD0031" />
+            </div>
+            <div className="face2">
+              <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+            </div>
+            <div className="face3">
+              <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+            </div>
+            <div className="face4">
+              <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+            </div>
+            <div className="face5">
+              <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+            </div>
+            <div className="face6">
+              <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <Loader type="pacman" />
+    </>
+  )
+}
+
+export default About
---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,79 @@
+import './index.scss'
+import LogoS from '../../assets/images/download.jpeg'
+import LogoSubtitle from '../../assets/images/ss1.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faLinkedin,
+  faGithub,
+  faYoutube,
+  faSkype,
+  faStackOverflow,
+  faTwitter,
+} from '@fortawesome/free-brands-svg-icons'
+import { faHome, faUser, faEnvelope, faSuitcase } from '@fortawesome/free-solid-svg-icons'
+import { Link, NavLink } from 'react-router-dom'
+
+const Sidebar = () => {
+  return (
+    <div className="nav-bar">
+      {/* <Link className="logo" to="/">
+        <img src={LogoS} alt="Logo" />
+        <img className="sub-logo" src={LogoSubtitle} alt="ss1" />
+      </Link> */}
+      <nav>
+        <NavLink exact="true" activeclassname="active" to="/">
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        </NavLink>
+        <NavLink activeclassname="active" className="about-link" to="/about">
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+        </NavLink>
+        <NavLink activeclassname="active" className="portfolio-link" to="/portfolio">
+          <FontAwesomeIcon icon={faSuitcase} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          activeclassname="active"
+          className="contact-link"
+          to="/contact"
+        >
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+        </NavLink>
+      </nav>
+      <ul>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/sahas-lakhote-280253216/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/sahasrocks"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://stackoverflow.com/users/19254759/sahas-lakhote"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <FontAwesomeIcon icon={faStackOverflow} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a href="https://twitter.com/sahaslakhote" rel="noreferrer" target="_blank">
+            <FontAwesomeIcon icon={faTwitter} color="#4d4d4e" />
+          </a>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default Sidebar
